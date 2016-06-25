109b53482f2e4578feb4d0b62466d22b7699fd3a
Talinona1 <timothe.golla@etu.unistra.fr>
2016-06-25 23:50:06 +0200

    Re-upload suite à bugs

---
+++ pages/contact.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device, initial-scale=1.0">
+<title>Contact</title>
+<link rel="stylesheet" href="../style/style.css" >
+<link rel="stylesheet" href="../font-awesome-4.6.3/css/font-awesome.css">
+</head>
+
+<body>
+    <header>
+         <nav class="menu">
+            <ul id="menu">
+                <li class="parler">
+                    <div class="img_menu"></div>
+                    <a href="presentation.html">Presentation</a>
+                </li>
+                <li class="bourse">
+                    <div class="img_menu"></div>
+                    <a href="experience.html">Experience</a>
+                </li>
+                <li class="epee">
+                    <div class="img_menu"></div>
+                    <a href="competences.html">Competences</a>
+                </li>
+                <li class="coeur">
+                    <div class="img_menu"></div>
+                    <a href="portfolio.html">Portfolio</a>
+                </li>
+                <li class="croix">
+                    <div class="img_menu"></div>
+                    <a href="contact.php">Contacts</a>
+            </ul>
+            <div id="unfold" class="menu_button">Menu <em class="fa fa-bars"></em></div>
+        </nav>
+    </header>
+    
+    <main>
+       <div class="wrapper">
+          <div>
+              <img src="../image/visage_blanc_clin_doeil.png" class="tetes">
+               <p>Si vous souhaitez me contacter, mes données sont en dessous. Vous pouvez également utiliser l’application en bas de page.</p>
+           </div>
+           
+           <div>
+               <ul>
+                   <li>Adresse : 1 rue Stiegelmann,
+                       67500 Haguenau</li>
+                   <li>Téléphone : 06.45.53.10.37</li>
+                   <li>Courriel : timothe.golla@gmail.com</li>
+                   <li><a href="">Téléchargez mon cv papier (A FAIRE)</a></li>
+               </ul>
+           </div>
+       </div>
+       
+       <?php
+/*
+Formulaire trouvé sur http://www.php-astux.info/script-formulaire-contact.php	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
+$destinataire = 'timothe.golla@gmail.com';
+ 
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui';
+ 
+// Action du formulaire (si votre page a des paramètres dans l'URL)
+// si cette page est index.php?page=contact alors mettez index.php?page=contact
+// sinon, laissez vide
+$form_action = 'contact.php';
+ 
+// Messages de confirmation du mail
+$message_envoye = "Votre message nous est bien parvenu !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+ 
+// Message d'erreur du formulaire
+$message_formulaire_invalide = "Vérifiez que tous les champs soient bien remplis et que l'email soit sans erreur.";
+ 
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+ 
+/*
+ * cette fonction sert à nettoyer et enregistrer un texte
+ */
+function Rec($text)
+{
+	$text = htmlspecialchars(trim($text), ENT_QUOTES);
+	if (1 === get_magic_quotes_gpc())
+	{
+		$text = stripslashes($text);
+	}
+ 
+	$text = nl2br($text);
+	return $text;
+};
+ 
+/*
+ * Cette fonction sert à vérifier la syntaxe d'un email
+ */
+function IsEmail($email)
+{
+	$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
+	return (($value === 0) || ($value === false)) ? false : true;
+}
+ 
+// formulaire envoyé, on récupère tous les champs.
+$nom     = (isset($_POST['nom']))     ? Rec($_POST['nom'])     : '';
+$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+$objet   = (isset($_POST['objet']))   ? Rec($_POST['objet'])   : '';
+$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+ 
+// On va vérifier les variables et l'email ...
+$email = (IsEmail($email)) ? $email : ''; // soit l'email est vide si erroné, soit il vaut l'email entré
+$err_formulaire = false; // sert pour remplir le formulaire en cas d'erreur si besoin
+ 
+if (isset($_POST['envoi']))
+{
+	if (($nom != '') && ($email != '') && ($objet != '') && ($message != ''))
+	{
+		// les 4 variables sont remplies, on génère puis envoie le mail
+		$headers  = 'From:'.$nom.' <'.$email.'>' . "\r\n";
+		//$headers .= 'Reply-To: '.$email. "\r\n" ;
+		//$headers .= 'X-Mailer:PHP/'.phpversion();
+ 
+		// envoyer une copie au visiteur ?
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.';'.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+ 
+		// Remplacement de certains caractères spéciaux
+		$message = str_replace("&#039;","'",$message);
+		$message = str_replace("&#8217;","'",$message);
+		$message = str_replace("&quot;",'"',$message);
+		$message = str_replace('&lt;br&gt;','',$message);
+		$message = str_replace('&lt;br /&gt;','',$message);
+		$message = str_replace("&lt;","&lt;",$message);
+		$message = str_replace("&gt;","&gt;",$message);
+		$message = str_replace("&amp;","&",$message);
+ 
+		// Envoi du mail
+		$num_emails = 0;
+		$tmp = explode(';', $cible);
+		foreach($tmp as $email_destinataire)
+		{
+			if (mail($email_destinataire, $objet, $message, $headers))
+				$num_emails++;
+		}
+ 
+		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+		{
+			echo '<p>'.$message_envoye.'</p>';
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>';
+		};
+	}
+	else
+	{
+		// une des 3 variables (ou plus) est vide ...
+		echo '<p>'.$message_formulaire_invalide.'</p>';
+		$err_formulaire = true;
+	};
+}; // fin du if (!isset($_POST['envoi']))
+ 
+if (($err_formulaire) || (!isset($_POST['envoi'])))
+{
+	// afficher le formulaire
+	echo '
+	<form id="contact" method="post" action="'.$form_action.'">
+	<fieldset><legend>Vos coordonnées</legend>
+		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" value="'.stripslashes($nom).'" tabindex="1" /></p>
+		<p><label for="email">Email :</label><input type="text" id="email" name="email" value="'.stripslashes($email).'" tabindex="2" /></p>
+	</fieldset>
+ 
+	<fieldset><legend>Votre message :</legend>
+		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" value="'.stripslashes($objet).'" tabindex="3" /></p>
+		<p><label for="message">Message :</label><textarea id="message" name="message" tabindex="4" cols="30" rows="8">'.stripslashes($message).'</textarea></p>
+	</fieldset>
+ 
+	<div style="text-align:center;"><input class="button" type="submit" name="envoi" value="" /></div>
+	</form>';
+};
+?>
+    </main>
+
+        <script src="../js/jquery-2.2.3.min.js"></script>
+        <script src="../js/main.js"></script>
+</body>
+</html>
